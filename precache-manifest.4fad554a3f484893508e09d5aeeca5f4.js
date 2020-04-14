4a264a39fb6ef12c7469bd46711a5a48ee1be23d
Thao Le <thaole1296@gmail.com>
2020-04-14 20:30:54 +0200

    pendu

---
+++ precache-manifest.4fad554a3f484893508e09d5aeeca5f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72aa191121418238f8aced9702ddedb2",
+    "url": "/pendu/index.html"
+  },
+  {
+    "revision": "e4844ca804ed7deca987",
+    "url": "/pendu/static/css/main.c013138a.chunk.css"
+  },
+  {
+    "revision": "8461a6247fa92ce572ea",
+    "url": "/pendu/static/js/2.d1198baf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pendu/static/js/2.d1198baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4844ca804ed7deca987",
+    "url": "/pendu/static/js/main.ff9e8663.chunk.js"
+  },
+  {
+    "revision": "ac293676bf4a2129741b",
+    "url": "/pendu/static/js/runtime-main.1a213c8d.js"
+  },
+  {
+    "revision": "10b6602b1298842fea2073ce5dcc45af",
+    "url": "/pendu/static/media/you_lose.10b6602b.png"
+  },
+  {
+    "revision": "7b8895b25cd2bc60f768ec8cfeb669cc",
+    "url": "/pendu/static/media/you_win.7b8895b2.png"
+  }
+]);
