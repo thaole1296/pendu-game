29ec5bb2e92d533f5c33fd7bca6b3730957a838d
Thao Le <thaole1296@gmail.com>
2020-04-16 16:22:39 +0200

    with answer

---
+++ precache-manifest.655585e3d97bb97bd95a924e13ddbc6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7117de2271980cd881ce0a488ab6b685",
+    "url": "/pendu-game/index.html"
+  },
+  {
+    "revision": "96201cf4098daf22b8b3",
+    "url": "/pendu-game/static/css/main.ce744d87.chunk.css"
+  },
+  {
+    "revision": "8461a6247fa92ce572ea",
+    "url": "/pendu-game/static/js/2.d1198baf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pendu-game/static/js/2.d1198baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96201cf4098daf22b8b3",
+    "url": "/pendu-game/static/js/main.93dbf317.chunk.js"
+  },
+  {
+    "revision": "54224a5e37df203606fa",
+    "url": "/pendu-game/static/js/runtime-main.e2dc8a4a.js"
+  },
+  {
+    "revision": "10b6602b1298842fea2073ce5dcc45af",
+    "url": "/pendu-game/static/media/you_lose.10b6602b.png"
+  },
+  {
+    "revision": "7b8895b25cd2bc60f768ec8cfeb669cc",
+    "url": "/pendu-game/static/media/you_win.7b8895b2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pendu-game/precache-manifest.01f4925986b2de39d0f346d7d362c192.js"
+  "/pendu-game/precache-manifest.655585e3d97bb97bd95a924e13ddbc6d.js"
 );
 
 self.addEventListener('message', (event) => {
